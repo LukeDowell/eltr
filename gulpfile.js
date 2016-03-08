ef355d9a4abfd632a9868b451537bf173054a682
Luke Dowell <lukedowell@gmail.com>
2016-03-08 11:17:00 -0600

    Removes sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,17 @@
 var gulp = require('gulp');
 var inject = require('gulp-inject');
 var angularFilesort = require('gulp-angular-filesort');
-var sass = require('gulp-sass');
 
 var paths = {
     index: './public/index.html',
     styles: [
         './public/stylesheets/**/*.css',
         './public/bower_components/angular-material/angular-material.css'],
-    sass: [
-        './public/stylesheets/**/*.scss',
-        './public/scripts/**/*.scss'],
     scripts: [
         './public/scripts/app/**/*.js',
         './public/scripts/components/**/*.js',
         './public/scripts/vendors/**/*.js']
 };
-
-gulp.task('sass', function() {
-    var options = {
-        outputStyle: 'compressed',
-        outFile: './public/stylesheets/style.css'
-    };
-
-    return gulp.src(paths.sass)
-        .pipe(sass(options).on('error', sass.logError))
-        .pipe(gulp.dest('./public/stylesheets'));
-});
 
 gulp.task('watch', function() {
     gulp.watch(paths.scripts, ['scripts']);
---
+++ public/scripts/app/race/race.controller.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ldowell on 3/7/16.
+ */
+angular.module('eltr')
+    .controller('MainController', ['$scope', 'IO', function($scope, IO) {
+        console.log(IO);
+    }]);
